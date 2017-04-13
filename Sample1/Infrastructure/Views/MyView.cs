71de9619e0ae03db65352bb159d802e839b433a9
Juan Carlos Vega Neira <jcvegan@live.com>
2017-04-12 21:01:56 -0500

    Sample1
    http://jcvegan.com/asp-mvc-personalizando-la-vista/

---
+++ Sample1/Infrastructure/Views/MyView.cs
@@ -0,0 +1,27 @@
+﻿namespace Sample1.Infrastructure.Views {
+    //Vistas sin modelo asignado
+    public abstract class MyView : System.Web.Mvc.WebViewPage {
+        //Atributos
+        private readonly string hello = "Hello World";
+
+        //Propiedades
+        public string Greeting {
+            get {
+                return hello;
+            }
+        }
+    }
+
+    //Vistas con modelo asignado
+    public abstract class MyView<TModel> : System.Web.Mvc.WebViewPage<TModel> {
+        //Atributos
+        private readonly string hello = "Hello World";
+
+        //Propiedades
+        public string Greeting {
+            get {
+                return hello;
+            }
+        }
+    }
+}
